21bc4c719315f49cc2283c5ca20c9ae6551dc0e5
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2025-09-11 13:09:30 +0700

    feat: convert png to webp

--- src/data/galleryItems/index.ts
+++ src/data/galleryItems/index.ts
@@ -4,7 +4,7 @@ const galleryItems: GalleryItem[] = [
   { src: "/activities/open-mind-pbl.webp", title: "Open Mind PBL 2025" },
   { src: "/activities/first-meet-2025.webp", title: "First Meet 2025" },
   { src: "/activities/fun-games.webp", title: "Fun Games First Meet 2025" },
-  { src: "/activities/team-discussion.png", title: "Team Discussion" },
+  { src: "/activities/team-discussion.webp", title: "Team Discussion" },
   { src: "/activities/photoshoots.webp", title: "Photoshoots" },
   { src: "/activities/hackathon.webp", title: "Hackathon Data Science" },
   // { src: "/logos/placeholder.png", title: "Kolaborasi Riset" },
